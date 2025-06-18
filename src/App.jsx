3e0ad3bfe4cffd61b31599392abcd74a2661dd9e
Charelk2 <Charelk@gmail.com>
2025-06-18 19:04:55 +0200

    Add missing Dashboard route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './screens/Home'
 import Session from './screens/Session'
+import Dashboard from './screens/Dashboard'
 import { ContentProvider } from './contexts/ContentProvider'
 import ErrorBanner from './components/ErrorBanner'
 import './App.css'
@@ -12,6 +13,7 @@ const App = () => (
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/session" element={<Session />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </ContentProvider>
   </BrowserRouter>
